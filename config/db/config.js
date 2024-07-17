d0d71deec08d6f1fbe082cd49fd4f8ffc1d2f23c
KAMAL RUIDAS <kamaldasbabu@gmail.com>
2024-07-17 23:43:26 +0530

    build files

---
+++ config/db/config.js
@@ -0,0 +1,11 @@
+// const mongoose = require("mongoose");
+// // eslint-disable-next-line no-undef
+// const url = process.env.MONGO_URI;
+// mongoose
+//   .connect(url)
+//   .then(() => {
+//     console.log("connected to db");
+//   })
+//   .catch(() => {
+//     console.log("failed db");
+//   });
